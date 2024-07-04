ceff98c4367fb7bfdc20988f4f6ad87ecaadf4a6
Thong1901 <166288258+Thong1901@users.noreply.github.com>
2024-07-04 18:28:24 +0700

    Add files via upload

---
+++ Doancanhan/Doancanhan/frm_DangKi.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Doancanhan
+{
+    partial class frm_DangKi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_tentk = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.btn_dangki = new System.Windows.Forms.Button();
+            this.btn_dong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(191, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Tài Khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(191, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // txt_tentk
+            // 
+            this.txt_tentk.Location = new System.Drawing.Point(321, 75);
+            this.txt_tentk.Name = "txt_tentk";
+            this.txt_tentk.Size = new System.Drawing.Size(157, 22);
+            this.txt_tentk.TabIndex = 2;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(321, 128);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(157, 22);
+            this.txt_pass.TabIndex = 3;
+            // 
+            // btn_dangki
+            // 
+            this.btn_dangki.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_dangki.Location = new System.Drawing.Point(301, 208);
+            this.btn_dangki.Name = "btn_dangki";
+            this.btn_dangki.Size = new System.Drawing.Size(75, 23);
+            this.btn_dangki.TabIndex = 4;
+            this.btn_dangki.Text = "Đăng Kí";
+            this.btn_dangki.UseVisualStyleBackColor = false;
+            this.btn_dangki.Click += new System.EventHandler(this.btn_dangki_Click);
+            // 
+            // btn_dong
+            // 
+            this.btn_dong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_dong.Location = new System.Drawing.Point(432, 208);
+            this.btn_dong.Name = "btn_dong";
+            this.btn_dong.Size = new System.Drawing.Size(75, 23);
+            this.btn_dong.TabIndex = 5;
+            this.btn_dong.Text = "Đóng";
+            this.btn_dong.UseVisualStyleBackColor = false;
+            this.btn_dong.Click += new System.EventHandler(this.btn_dong_Click);
+            // 
+            // frm_DangKi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_dong);
+            this.Controls.Add(this.btn_dangki);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_tentk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(800, 80);
+            this.Name = "frm_DangKi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "frm_DangKi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_tentk;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Button btn_dangki;
+        private System.Windows.Forms.Button btn_dong;
+    }
+}
---
+++ Doancanhan/Doancanhan/frm_Dangnhap.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Doancanhan
+{
+    partial class frm_Dangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_tentk = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.btn_dangki = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(560, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(506, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // txt_tentk
+            // 
+            this.txt_tentk.Location = new System.Drawing.Point(646, 128);
+            this.txt_tentk.Name = "txt_tentk";
+            this.txt_tentk.Size = new System.Drawing.Size(190, 22);
+            this.txt_tentk.TabIndex = 3;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(646, 180);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(190, 22);
+            this.txt_pass.TabIndex = 4;
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Location = new System.Drawing.Point(646, 232);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(98, 23);
+            this.btn_dangnhap.TabIndex = 5;
+            this.btn_dangnhap.Text = "Đăng Nhâp";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // btn_dangki
+            // 
+            this.btn_dangki.Location = new System.Drawing.Point(765, 232);
+            this.btn_dangki.Name = "btn_dangki";
+            this.btn_dangki.Size = new System.Drawing.Size(98, 23);
+            this.btn_dangki.TabIndex = 6;
+            this.btn_dangki.Text = "Đăng Kí";
+            this.btn_dangki.UseVisualStyleBackColor = true;
+            this.btn_dangki.Click += new System.EventHandler(this.btn_dangki_Click);
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Location = new System.Drawing.Point(646, 280);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_thoat.TabIndex = 7;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click);
+            // 
+            // frm_Dangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1153, 534);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_dangki);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_tentk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.IsMdiContainer = true;
+            this.Name = "frm_Dangnhap";
+            this.Text = "Dăng Kí";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_tentk;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.Button btn_dangki;
+        private System.Windows.Forms.Button btn_thoat;
+    }
+}
+
